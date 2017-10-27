2a6284c10a71ba20decbb9df26a750989c924a56
RachelleT <rache_trotman@hotmail.com>
2017-10-27 14:44:00 -0400

    Javascript File

--- maze.js
+++ maze.js
@@ -1,45 +1,38 @@
 window.onload = function () {
-	  main();
-	};
-	
-
-	function main() 
-	{
 		var boundary1 = document.getElementById("boundary1");
 		boundary1.onmouseover = function() {
 			this.setAttribute("class", "boundary youlose");
 		};
 		
 		var boundary = document.querySelectorAll(".boundary");
-		var i = 0;
 		var a = 0;
-		var g = 0;
-		for ( i = 0; i < boundary.length; i++)
+		var b = 0;
+		var tracker = 0;
+		for ( a = 0; a < boundary.length; a++)
 		{
-			boundary[i].onmouseover = function(){
-				for ( a = 0; a< boundary.length-1;a++){
-					boundary[a].setAttribute("class","boundary youlose");
+			boundary[a].onmouseover = function(){
+				for ( b = 0; b< boundary.length-1;b++){
+					boundary[b].setAttribute("class","boundary youlose");
 					
 				}
 				g++;
 				document.getElementById("status").innerHTML = "You lose";
 			}
 		}
-		var end = document.getElementById("end");
-		end.onmouseover = function(){
-			if (g == 0)
+		var e = document.getElementById("end");
+		e.onmouseover = function(){
+			if (tracker == 0)
 			{
-				//window.alert("You Win");
 				document.getElementById("status").innerHTML = "You Win";
 			};
 		};
 		
-		var start = document.getElementById("start");
-		start.onclick = function(){
-			g=0;
+		var s = document.getElementById("start");
+		s.onclick = function(){
+			tracker=0;
 			document.getElementById("status").innerHTML = "Move your mouse over the \"S\" to begin.";
-			for ( i = 0; i < boundary.length-1;i++) {
-				boundary[i].setAttribute("class","boundary boundary");
+			for ( a = 0; a < boundary.length-1;a++) {
+				boundary[a].setAttribute("class","boundary boundary");
 			};
 			var maze = document.getElementById("maze");
 			document.onmousemove = function (event) {
@@ -52,9 +45,9 @@ window.onload = function () {
 				var t = maze.offsetTop;
 				if ((x < l)||(x < r))
 				{
-					for (a = 0; a< boundary.length ; a++)
+					for (b = 0; b< boundary.length ; b++)
 					{
-						boundary[a].setAttribute("class","boundary youlose");
+						boundary[b].setAttribute("class","boundary youlose");
 					}
 				};
 			};
