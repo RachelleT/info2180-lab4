227b5a17c27b99118054575dfac7c539c6b01106
RachelleT <rache_trotman@hotmail.com>
2017-10-27 14:56:07 -0400

    Javascript File

--- maze.js
+++ maze.js
@@ -1,25 +1,22 @@
 window.onload = function () {
-		var boundary1 = document.getElementById("boundary1");
+		let boundary1 = document.getElementById("boundary1");
 		boundary1.onmouseover = function() {
 			this.setAttribute("class", "boundary youlose");
 		};
-		
-		var boundary = document.querySelectorAll(".boundary");
-		var a = 0;
-		var b = 0;
-		var tracker = 0;
-		for ( a = 0; a < boundary.length; a++)
+		let boundary = document.querySelectorAll(".boundary");
+		let tracker = 0;
+		for (let a = 0; a < boundary.length; a++)
 		{
 			boundary[a].onmouseover = function(){
-				for ( b = 0; b< boundary.length-1;b++){
+				for (let b = 0; b< boundary.length-1;b++){
 					boundary[b].setAttribute("class","boundary youlose");
 					
 				}
-				g++;
-				document.getElementById("status").innerHTML = "You lose";
+				tracker++;
+				document.getElementById("status").innerHTML = "You Lose";
 			}
 		}
-		var e = document.getElementById("end");
+		let e = document.getElementById("end");
 		e.onmouseover = function(){
 			if (tracker == 0)
 			{
@@ -27,32 +24,30 @@ window.onload = function () {
 			};
 		};
 		
-		var s = document.getElementById("start");
+		let s = document.getElementById("start");
 		s.onclick = function(){
 			tracker=0;
 			document.getElementById("status").innerHTML = "Move your mouse over the \"S\" to begin.";
-			for ( a = 0; a < boundary.length-1;a++) {
+			for (let a = 0; a < boundary.length-1;a++) {
 				boundary[a].setAttribute("class","boundary boundary");
 			};
-			var maze = document.getElementById("maze");
+			let maze = document.getElementById("maze");
 			document.onmousemove = function (event) {
-				var x = event.clientX;
-				var y = event.clientY;
-				var l = maze.offsetLeft;
-				var r = maze.offsetRight;
-				var h = maze.offsetHeight;
-				var w = maze.offsetWidth;
-				var t = maze.offsetTop;
+				let x = event.clientX;
+				let y = event.clientY;
+				let l = maze.offsetLeft;
+				let r = maze.offsetRight;
+				let h = maze.offsetHeight;
+				let w = maze.offsetWidth;
+				let t = maze.offsetTop;
 				if ((x < l)||(x < r))
 				{
-					for (b = 0; b< boundary.length ; b++)
+					for (let b = 0; b< boundary.length ; b++)
 					{
 						boundary[b].setAttribute("class","boundary youlose");
 					}
 				};
 			};
 			
-		};
-		
-		
+		};	
 	};	
